6ad05268de48dfc474031cf0eb1171cd16afbe9e
Adachi Masakazu <marscode48@gmail.com>
2025-06-17 10:35:03 +0900

    feat: ブログ投稿ページ用の single.php を作成

---
+++ src/single.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying single blog posts
+ *
+ * @package PRO_SHOP_WAVE
+ */
+?>
+
+<?php get_header(); ?>
+
+<main class="single-blog section">
+  <div class="single-blog__inner">
+    <header class="single-blog__header">
+      <h1 class="single-blog__title section__title"><?php the_title(); ?></h1>
+      <div class="single-blog__meta">
+        <time datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
+        <span>｜</span>
+        <span><?php the_category(', '); ?></span>
+      </div>
+      <?php if (has_post_thumbnail()) : ?>
+        <div class="single-blog__thumbnail">
+          <?php the_post_thumbnail('large'); ?>
+        </div>
+      <?php endif; ?>
+    </header>
+
+    <div class="single-blog__content">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
