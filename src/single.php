b57ba9cea681cd21f5fd5d9180a179a385e47953
Adachi Masakazu <marscode48@gmail.com>
2025-10-28 13:01:12 +0900

    feat: ブログに single-blog__body クラスを追加して背景スタイルを記事のみに適用

--- src/single.php
+++ src/single.php
@@ -11,20 +11,21 @@
 <main class="single-blog section">
   <div class="single-blog__inner l-container l-container--narrow">
     <?php get_template_part('template-parts/breadcrumb/breadcrumb'); ?>
-    <header class="single-blog__header">
-      <h1 class="single-blog__title section__title"><?php the_title(); ?></h1>
-      <div class="single-blog__meta">
-        <time datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-        <span>｜</span>
-        <span><?php the_category(', '); ?></span>
-      </div>
-      <?php if (has_post_thumbnail()) : ?>
-        <div class="single-blog__thumbnail">
-          <?php the_post_thumbnail('large'); ?>
+    <div class="single-blog__body">
+      <header class="single-blog__header">
+        <h1 class="single-blog__title section__title"><?php the_title(); ?></h1>
+        <div class="single-blog__meta">
+          <time datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
+          <span>｜</span>
+          <span><?php the_category(', '); ?></span>
         </div>
-      <?php endif; ?>
-    </header>
-
+        <?php if (has_post_thumbnail()) : ?>
+          <div class="single-blog__thumbnail">
+            <?php the_post_thumbnail('large'); ?>
+          </div>
+        <?php endif; ?>
+      </header>
+    </div>
     <div class="single-blog__content">
       <?php the_content(); ?>
     </div>
