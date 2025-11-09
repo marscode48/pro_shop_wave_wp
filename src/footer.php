e472cc02b687314cfe2bf259017549d9ea41676a
Adachi Masakazu <marscode48@gmail.com>
2025-11-09 14:24:16 +0900

    feat: 利用規約ページを新規実装し、共通リード文・本文スタイルを適用

--- src/footer.php
+++ src/footer.php
@@ -50,7 +50,7 @@
 
         <!-- ポリシーなど -->
         <div class="footer__legal">
-          <a href="#">利用規約</a>
+          <a href="<?php echo esc_url(home_url('/terms/')); ?>">利用規約</a>
           <a href="#">プライバシーポリシー</a>
           <a href="#">特商法表記</a>
         </div>
