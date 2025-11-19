f1ca7f50509f5a9667b56c771fb72ce68e493ea1
Adachi Masakazu <marscode48@gmail.com>
2025-11-19 14:24:39 +0900

    refactor(woocommerce): 関連・おすすめ商品の見出しをWooCommerce標準に統一しハードコードを削除

--- src/woocommerce/single-product/related.php
+++ src/woocommerce/single-product/related.php
@@ -31,8 +31,7 @@ if ($related_products) : ?>
 		?>
 			<div class="products__header">
 				<h2 class="section__title fadeup">
-					<span class="products__header--top"><?php echo esc_html($heading); ?></span>
-					<span class="products__header--bottom">関連商品</span>
+					<?php echo esc_html($heading); ?>
 				</h2>
 			</div>
 		<?php endif; ?>
